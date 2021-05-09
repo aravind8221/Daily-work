75ae6baa7be8968c4d3357ba6b34c56e4a629162
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-05-09 18:24:13 +0530

    Initial commit

---
+++ 9-5-2021/js/shakespears.js
@@ -0,0 +1,25 @@
+$(() => {
+    $('#selected-plays')
+        .addClass('flex');
+    $('#selected-plays > li')
+        .addClass('horizontal');
+    $('#selected-plays li:not(.horizontal)')
+        .addClass('sub-level')
+    $('a[href^="mailto:"]')
+        .addClass('mailto');
+    $('a[href$=".pdf"]')
+        .addClass('pdflink');
+    $('a[href^="http"][href*="henry"]')
+        .addClass('henrylink');
+    $('tr:nth-child(odd)').addClass('alt');
+    
+    $('td:contains(Henry)')
+        .parent()
+        .children()
+        .addClass('highlight');
+    $('a')
+        .filter((i, a) =>
+            a.hostname && a.hostname !== location.hostname
+        )
+        .addClass('external');
+});
