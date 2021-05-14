bd453028b5e674507d039ebcd80fa998b17f4643
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-05-15 00:25:21 +0530

    Create forLoop.js

---
+++ 14-5-2021/typescript/forLoop.js
@@ -0,0 +1,8 @@
+var marks = [30, 50, 70, 45, 58, 36];
+for (var m in marks) {
+    console.log(marks[m]);
+}
+for (var _i = 0, marks_1 = marks; _i < marks_1.length; _i++) {
+    var n = marks_1[_i];
+    console.log(n);
+}
