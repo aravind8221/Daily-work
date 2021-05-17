c9dd7781341802f13d4341bccbca3e5f857fac81
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-05-17 09:13:15 +0530

    Create employee.mock.ts

---
+++ 14-5-2021/employee1-management/src/app/employee.mock.ts
@@ -0,0 +1,9 @@
+import {Employee} from './model/employee';
+
+export const EMPLOYEES: Employee[]=[
+{id:101, name:"Aravind", age:22, address:"Anantapur", company:"CTS"},
+{id:102, name:"Naveen", age:22, address:"Banglore", company:"Facebook"},
+{id:103, name:"Abhi", age:21, address:"Hyderabad", company:"Google"},
+{id:104, name:"Patan", age:23, address:"Pune", company:"Microsoft"},
+{id:105, name:"Suresh", age:24, address:"Chennai", company:"Amazon"},
+];
