4f6fcccf06c45edcbede8ad53fb64cd42c24300b
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-05-20 11:34:42 +0530

    Update app.module.ts

--- 15-5-2021/employee1-management/src/app/app.module.ts
+++ 15-5-2021/employee1-management/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
-import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { StructureDirectiveComponent } from './structure-directive/structure-directive.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { HttpClientModule } from '@angular/common/http'
+import { AddEmployeeComponent } from './employee/add-employee/add-employee.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UpdateEmployeeComponent } from './employee/update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +19,18 @@ import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.compon
     EmployeeComponent,
     InterpolationComponent,
     PropertyBindingComponent,
-    EventBindingComponent,
-    TwoWayBindingComponent,
+    EventBindingComponent,   
+    StructureDirectiveComponent,
+    UserFormComponent,
+    AddEmployeeComponent,
+    UpdateEmployeeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
