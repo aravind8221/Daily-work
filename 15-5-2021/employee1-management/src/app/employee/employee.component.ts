7e9f0f994c421dd25a9fd13c39c6be62fafc4084
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-05-20 11:40:03 +0530

    Update employee.component.ts

--- 15-5-2021/employee1-management/src/app/employee/employee.component.ts
+++ 15-5-2021/employee1-management/src/app/employee/employee.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { EMPLOYEES } from '../employee.mock';
 
+import { Employee } from './../model/employee';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../service/employee.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent  {
-  employees=EMPLOYEES;
+export class EmployeeComponent  implements OnInit{
+  employees: Employee[];
+  
+  constructor(private employeeService:EmployeeService, private router: Router) {}
+    ngOnInit(): void {
+      this.employeeService.getAllEmployees().subscribe(data => {
+          this.employees=data;
+      })
+    }
+    updateEmployee(employee: Employee): void{
+      localStorage.removeItem('id');
+      localStorage.setItem('id', employee.id.toString());
+      this.router.navigate(['update-employee']);
+      
+    }
+    deleteEmployee(employee: Employee): void{
+      this.employeeService.deleteEmployee(employee.id).subscribe();
+      this.employees = this.employees.filter(e => e !== employee);
+    }
+
 
 }
