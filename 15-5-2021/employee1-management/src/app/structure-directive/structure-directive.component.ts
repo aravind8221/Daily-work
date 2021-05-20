c58d49e900c49227a1212be0f7c8d099b886cd34
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-05-20 11:52:03 +0530

    Create structure-directive.component.ts

---
+++ 15-5-2021/employee1-management/src/app/structure-directive/structure-directive.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-structure-directive',
+  templateUrl: './structure-directive.component.html',
+  styleUrls: ['./structure-directive.component.css']
+})
+export class StructureDirectiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  name = ["aravind", "bharath", "vasu"];
+
+}
