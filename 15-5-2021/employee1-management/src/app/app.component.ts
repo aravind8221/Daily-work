d2237677dbea823b461b853b401d5b451fcd73c8
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-05-20 11:34:01 +0530

    Update app.component.ts

--- 15-5-2021/employee1-management/src/app/app.component.ts
+++ 15-5-2021/employee1-management/src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  count=105
   title = 'Employee';
   name = 'Aravind';
+  getCount(counter:number): void{
+    this.count=counter;
+    }
+    ngOnInit(): void{
+      this.getFromChild(this.count);
+    }
+    getFromChild(event: number): void {
+      this.count=event;
+    }
 }
