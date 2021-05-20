a6cf0ac29d993b980a95ff3b9c04371aa14df976
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-05-20 11:35:27 +0530

    Update employee.mock.ts

--- 15-5-2021/employee1-management/src/app/employee.mock.ts
+++ 15-5-2021/employee1-management/src/app/employee.mock.ts
@@ -1,9 +1,9 @@
 import {Employee} from './model/employee';
 
 export const EMPLOYEES: Employee[]=[
-{id:101, name:"Aravind", age:22, address:"Anantapur", company:"CTS"},
-{id:102, name:"Naveen", age:22, address:"Banglore", company:"Facebook"},
-{id:103, name:"Abhi", age:21, address:"Hyderabad", company:"Google"},
-{id:104, name:"Patan", age:23, address:"Pune", company:"Microsoft"},
-{id:105, name:"Suresh", age:24, address:"Chennai", company:"Amazon"},
+{id:101, password:"password", name:"Aravind", age:22, address:"Anantapur", company:"CTS"},
+{id:102, password:"password", name:"Naveen", age:22, address:"Banglore", company:"Facebook"},
+{id:103, password:"password", name:"Abhi", age:21, address:"Hyderabad", company:"Google"},
+{id:104, password:"password", name:"Patan", age:23, address:"Pune", company:"Microsoft"},
+{id:105, password:"password", name:"Suresh", age:24, address:"Chennai", company:"Amazon"},
 ];
