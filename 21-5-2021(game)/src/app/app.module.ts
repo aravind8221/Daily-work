eda75cf9fc9d98914a7d868db92395625067f168
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-05-22 16:58:41 +0530

    Create app.module.ts

---
+++ 21-5-2021(game)/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
+import { StructureDirectiveComponent } from './structure-directive/structure-directive.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { HttpClientModule } from '@angular/common/http'
+import { AddEmployeeComponent } from './employee/add-employee/add-employee.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UpdateEmployeeComponent } from './employee/update-employee/update-employee.component';
+import { GameComponent } from './game/game.component';
+import { OddNumberComponent } from './odd-number/odd-number.component';
+import { EvenNumberComponent } from './even-number/even-number.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    InterpolationComponent,
+    PropertyBindingComponent,
+    EventBindingComponent,   
+    StructureDirectiveComponent,
+    UserFormComponent,
+    AddEmployeeComponent,
+    UpdateEmployeeComponent,
+    GameComponent,
+    OddNumberComponent,
+    EvenNumberComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
